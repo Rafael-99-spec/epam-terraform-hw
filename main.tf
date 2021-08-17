6a763edb65b6f682acda21bfadbc2faa466b97ee
Rafael-99-spec <60171771+Rafael-99-spec@users.noreply.github.com>
2021-08-17 10:02:13 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-2"
-  access_key = "AKIAXU6QPVUUHD4HKPQS"
-  secret_key = "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"
+  access_key = "AKIAXU6QPVUUK3JN3BMG"
+  secret_key = "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"
 }
 
 resource "aws_instance" "web-app" {
@@ -85,37 +85,5 @@ resource "aws_security_group" "allow_http" {
     to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
-  }
-}
-
-# Create a new load balancer
-resource "aws_elb" "bar" {
-  name               = "foobar-terraform-elb"
-  availability_zones = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  security_groups    = [aws_security_group.allow_http.id]
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-
-  health_check {
-    healthy_threshold   = 10
-    unhealthy_threshold = 2
-    timeout             = 5
-    target              = "HTTP:80/"
-    interval            = 30
-  }
-
-  instances                   = [aws_instance..id]
-  cross_zone_load_balancing   = true
-  idle_timeout                = 400
-  connection_draining         = true
-  connection_draining_timeout = 400
-
-  tags = {
-    Name = "foobar-terraform-elb"
   }
 }
